6765b8a124e66d25379dd2fb134bf5425a3755f8
Filipe Fróes <fr.froes@gmail.com>
2018-07-14 19:37:18 -0300

    Added toogle mechanism for from support on mobile

--- src/app/views/Dashboard/index.js
+++ src/app/views/Dashboard/index.js
@@ -7,10 +7,22 @@ import { PlaceholderList } from '../../components';
 import './index.css'
 
 export class Dashboard extends Component {
+
+  state = {
+    isFormVisible: false,
+  }
+
+  _toogleFormVisible = () => {
+    this.setState({
+      isFormVisible: !this.state.isFormVisible
+    })
+  }
   
   render() {
     
     const { header, data, form } = this.props;
+    const { isFormVisible } = this.state;
+    const arrowPosition = isFormVisible ? 'left' : 'right';
 
     return (
       <Container className="dashboard">
@@ -21,17 +33,18 @@ export class Dashboard extends Component {
             primary 
             basic 
             size="small" 
-            content={`Create ${header.label}`}
-            icon='right arrow' labelPosition='right'
+            content={isFormVisible? `${header.label} list` : `Create ${header.label}`}
+            icon={`${arrowPosition} arrow`} labelPosition={arrowPosition}
+            onClick={this._toogleFormVisible}
           />
         </Header>
         <div className="content">
-          <div>
+          <div className={isFormVisible? 'hidden-mobile' : ''}>
             <Segment>
               {data.isReady? data.component : <PlaceholderList length={3}/>}
             </Segment>
           </div>
-          <div>
+          <div className={!isFormVisible? 'hidden-mobile' : ''} >
               {form.component}
           </div>
         </div>
